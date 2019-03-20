883f3047a2f12a17981770cd7d1ebe9b5fd3fed9
George Mavroeidis <gdmavroeidis@hotmail.com>
2019-03-20 11:54:22 -0400

    Update CharacterReader.java

--- CharacterReader.java
+++ CharacterReader.java
@@ -13,53 +13,57 @@ public class CharacterReader {
 
 	public static void main(String[] args) {
 		
+		// Path to file
 		String filename = "C:\\Users\\G\\Desktop\\chars.txt";
 		
+		// Initializing objects to null
 		File file = new File(filename);
 		String str = null;
 		Scanner keyboard = null;
 		Scanner input = null;
-		boolean tryagain = true;
 		
-		do {
-			try {
-				PrintWriter output = new PrintWriter(new FileOutputStream(file));
-				keyboard = new Scanner(System.in);
+		try {
+			PrintWriter output = new PrintWriter(new FileOutputStream(file));
+			keyboard = new Scanner(System.in);
 				
-				System.out.println("Welcome to the Character Reader and Writer generator!");
+			// User is prompted to enter string to file
+			System.out.println("Welcome to the Character Reader and Writer generator!");
+			System.out.println();
+			System.out.print("Enter string to write on file: ");
+			str = keyboard.nextLine();
+			
+			// Writin to file and closing writer
+			output.println(str);
+			output.close();
+			
+			// Reading file
+			input = new Scanner(new FileInputStream(file));
+			
+			// Iterate through line(s)
+			while (input.hasNextLine()) {
+				// Read line with scanner
+				String characters = input.nextLine();
+				
 				System.out.println();
-				System.out.print("Enter string to write on file: ");
-				str = keyboard.nextLine();
-				
-				output.println(str);
-				output.close();
-				
-				input = new Scanner(new FileInputStream(file));
-				
-				while (input.hasNextLine()) {
-					String characters = input.nextLine();
-					
-					System.out.println();
-					System.out.println("Characters found: ");
-					for (int i = 0; i < characters.length(); i++) {
-						char c = characters.charAt(i);
-						if (!Character.isDigit(c))
-							System.out.println(c + " - " + (int)c);
-						else
-							System.out.println(c + " - No ASCII for numbers");
-					}
+				System.out.println("Characters found: ");
+				// Iterate through characters of line
+				for (int i = 0; i < characters.length(); i++) {
+					char c = characters.charAt(i);
+					// Print character with ASCII code, if provided
+					if (!Character.isDigit(c))
+						System.out.println(c + " - " + (int)c);
+					else
+						System.out.println(c + " - No ASCII for numbers");
 				}
-				tryagain = false;
-				
-			} catch (FileNotFoundException e) {
-				System.out.println("File not found. Please restart program.");
-				System.exit(0);
-			} finally {
-				System.out.println();
-				System.out.println("Program is closing. Have a nice day!");
 			}
-		} while (tryagain == true);
+		// In case file is not properly created, catch exception and quit
+		} catch (FileNotFoundException e) {
+			System.out.println("File not found. Please restart program.");
+			System.exit(0);
+		} finally {
+			System.out.println();
+			System.out.println("Program is closing. Have a nice day!");
+		}
 
 	}
-
 }
